db96f25539cd72fbff412cada8666885cebe3b34
AnshikaaRathore <anshikaarthr@gmail.com>
2025-09-04 16:03:20 +0530

    Update As2-q3a.cpp

--- As2-q3a.cpp
+++ As2-q3a.cpp
@@ -2,19 +2,27 @@
 #include <vector>
 using namespace std;
 
-
-
 int findMissingLinear(vector<int>& arr, int n) {
     int totalSum = n * (n + 1) / 2; 
     int arraySum = 0;
+
     for (int num : arr)
         arraySum += num; 
+
     return totalSum - arraySum; 
 }
 
 int main() {
-    vector<int> arr = {1, 2, 3, 5, 6}; 
-    int n = 6;
-    cout << "Missing number: " << findMissingLinear(arr, n);
+    int n;
+    cout << "Enter value of n (range 1 to n): ";
+    cin >> n;
+
+    vector<int> arr(n - 1);
+    cout << "Enter " << n - 1 << " elements of the array (sorted):"<<endl;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Missing number: " << findMissingLinear(arr, n) << endl;
     return 0;
 }
